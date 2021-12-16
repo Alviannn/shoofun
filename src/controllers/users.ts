3871d80d460b557928bbf7f226873c745fa65032
Alviannn <alvianndq@gmail.com>
2021-12-16 12:00:08 +0700

    Added function to check user existence and such

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -4,24 +4,90 @@ import { psql } from '../main';
 
 const ROUNDS = 12;
 
+type UserOptions = {
+    id?: number,
+    name?: string,
+    email?: string,
+};
+
 /**
- * @param user The incomplete user object
+ * Registers a user to the database
+ *
+ * @param user the user object recevied from client (incomplete)
  */
-export async function register(user: User) {
-    // since the password isn't yet hashed, we're hashing it
-    user.password = await bcrypt.hash(user.password, ROUNDS);
+export async function register(user: User): Promise<boolean> {
+    // TODO: validate object
+
+    const userExist = await doesUserExist({ email: user.email });
+    if (!userExist) {
+        return false;
+    }
+
+    // the password from user isn't hashed
+    const hashedPwd = await bcrypt.hash(user.password, ROUNDS);
 
     await psql.query(
         `INSERT INTO users
             (name, display, email, password, phone)
         VALUES
             ($1, $2, $3, $4, $5);`,
-        [user.name, user.display, user.password, user.phone]
+        [user.name, user.display, hashedPwd, user.phone]
     );
+
+    return true;
 }
 
-export async function findUser(idOrName: number | string) {
-    // TODO:
+/**
+ * Finds a user data based on a specific property.
+ *
+ * Although this is a bit expensive to be used for only checking existence,
+ * there's an alternative called {@link doesUserExist}.
+ *
+ * @param options the options to find the user based on specific property.
+ * @returns an array of user, will be empty if not found.
+ */
+export async function findUser(
+    options: UserOptions): Promise<User[]> {
+
+    if (!options.id || !options.email || !options.name) {
+        return [];
+    }
+
+    // find a key that has a value
+    const param = Object.keys(options)
+        .find((key) => Boolean(options[key]));
+
+    const { rows } = await psql.query(
+        `SELECT * FROM users WHERE ${param} = $1;`,
+        [param]
+    );
+
+    return rows as User[];
+}
+
+/**
+ * This is the cheaper version of the function to check the user existence.
+ *
+ * Checking user existence using the {@link findUser} can be expensive
+ * and bad practice since it grabs the entire user property.
+ *
+ * @param options the options to find the user based on specific property.
+ */
+export async function doesUserExist(options: UserOptions): Promise<boolean> {
+    if (!options.id || !options.email || !options.name) {
+        return;
+    }
+
+    // find a key that has a value
+    const param = Object.keys(options)
+        .find((key) => Boolean(options[key]));
+
+    const { rows } = await psql.query(
+        `SELECT id FROM users WHERE ${param} = $1;`,
+        [param]
+    );
+
+    return Boolean(rows);
 }
 
 /**
