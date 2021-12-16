3f82ff884d7afdb271ca34ba4e675442de9c97ff
Alviannn <alvianndq@gmail.com>
2021-12-16 11:40:18 +0700

    Test login should be working properly

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -24,6 +24,22 @@ export async function findUser(idOrName: number | string) {
     // TODO:
 }
 
-export async function testLogin(user: User) {
-    user.password = await bcrypt.hash(user.password, ROUNDS);
+/**
+ * Tests user validity by their existence and password.
+ *
+ * @param user the user object recevied from client (incomplete)
+ * @returns `true` if the user is valid, `false` is otherwise.
+ */
+export async function testLogin(user: User): Promise<boolean> {
+    if (!user.id) {
+        return false;
+    }
+
+    const { rows } = await psql.query(
+        'SELECT password FROM users WHERE email = $1;',
+        [user.email]
+    );
+
+    // the password from user object isn't hashed, we can just check it.
+    return bcrypt.compare(user.password, rows[0]);
 }
