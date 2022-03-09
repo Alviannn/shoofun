0c94c4f09a4af55b85d4d02af404000f427ed5a8
Alviannn <alvianndq@gmail.com>
2022-03-09 14:53:40 +0700

    Fix auth middleware not responding on empty headers

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -7,20 +7,25 @@ import httpStatus from 'http-status-codes';
 
 function authHandler(req: Request, res: Response, next: NextFunction) {
     const header = req.header('authorization');
+    const unauthorizedRes = makeResponse({
+        success: false,
+        statusCode: httpStatus.UNAUTHORIZED,
+        message: "You don't have an account session"
+    });
+
     if (!header || !header.includes('Bearer')) {
-        return;
+        return unauthorizedRes.send(res);
     }
 
     const token = header.split(' ')[1];
     try {
-        jwt.verify(token, config.jwt.accessSecret, { complete: true });
+        jwt.verify(token, config.jwt.accessSecret, {
+            complete: true
+        });
+
         return next();
     } catch (err) {
-        return makeResponse({
-            success: false,
-            statusCode: httpStatus.UNAUTHORIZED,
-            message: "You don't have an account session"
-        }).send(res);
+        return unauthorizedRes.send(res);
     }
 }
 
