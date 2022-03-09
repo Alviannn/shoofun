9da327b0e7cdf454f520429c7756bb1b9538cca0
Alviannn <alvianndq@gmail.com>
2022-03-09 14:01:22 +0700

    Finish error on auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,21 +1,27 @@
-import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
+import { makeResponse } from '../utils/api.util';
 
-function authHandler(req: Request, _: Response, next: NextFunction) {
+import jwt from 'jsonwebtoken';
+import config from '../configs/config';
+import httpStatus from 'http-status-codes';
+
+function authHandler(req: Request, res: Response, next: NextFunction) {
     const header = req.header('authorization');
     if (!header || !header.includes('Bearer')) {
         return;
     }
 
     const token = header.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!, (err) => {
-        if (err) {
-            // TODO: redirect user to login page
-            return console.error(err.message);
-        }
-
-        next();
-    });
+    try {
+        jwt.verify(token, config.jwt.accessSecret, { complete: true });
+        return next();
+    } catch (err) {
+        return makeResponse({
+            success: false,
+            statusCode: httpStatus.UNAUTHORIZED,
+            message: "You don't have an account session"
+        }).send(res);
+    }
 }
 
 export default authHandler;
