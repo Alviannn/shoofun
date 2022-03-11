580e0dd27184bb06612c7ca769b013323182d13a
Alviannn <alvianndq@gmail.com>
2022-03-11 13:42:19 +0700

    Fix forgotten API response sending changes

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,13 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
-import { makeResponse } from '../utils/api.util';
 import { StatusCodes } from 'http-status-codes';
+import { sendResponse } from '../utils/api.util';
 
 import jwt from 'jsonwebtoken';
 import config from '../configs/config';
 
 function authHandler(req: Request, res: Response, next: NextFunction) {
     const header = req.header('authorization');
-    const unauthorizedRes = makeResponse({
+    const unauthorizedRes = sendResponse(res, {
         success: false,
         statusCode: StatusCodes.UNAUTHORIZED,
         message: "You don't have an account session"
