4f843c79bae6da2f358766b7d8b19462cf0992ae
Alviannn <alvianndq@gmail.com>
2022-03-11 19:48:21 +0700

    Fix auth middleware always responding 401

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -7,14 +7,13 @@ import config from '../configs/config';
 
 function authHandler(req: Request, res: Response, next: NextFunction) {
     const header = req.header('authorization');
-    const unauthorizedRes = sendResponse(res, {
-        success: false,
-        statusCode: StatusCodes.UNAUTHORIZED,
-        message: "You don't have an account session"
-    });
 
     if (!header || !header.includes('Bearer')) {
-        return unauthorizedRes.send(res);
+        return sendResponse(res, {
+            success: false,
+            statusCode: StatusCodes.UNAUTHORIZED,
+            message: "You don't have an account session"
+        });
     }
 
     const token = header.split(' ')[1];
@@ -25,7 +24,11 @@ function authHandler(req: Request, res: Response, next: NextFunction) {
 
         return next();
     } catch (err) {
-        return unauthorizedRes.send(res);
+        return sendResponse(res, {
+            success: false,
+            statusCode: StatusCodes.UNAUTHORIZED,
+            message: "You don't have an account session"
+        });
     }
 }
 
